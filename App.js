c1947d062fdd9a92388198ceef90dbde68d69965
qianzhiwei <380659419@qq.com>
2023-02-02 21:30:57 +0800

    first

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import registerNNPushToken from 'native-notify';
+import { registerRootComponent } from 'expo';
+
 
 export default function App() {
+  registerNNPushToken(6159, 'ih3rzQPUq1PtPdISELoVPd');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello, NBA Score Notification when one of team score &gt;=100</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 5,
+    color: 'green',
+  }
 });
+registerRootComponent(App);
