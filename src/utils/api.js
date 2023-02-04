1942cb45824a1a91ff4201e6b28ac2133d4c0476
qianzhiwei <380659419@qq.com>
2023-02-04 21:15:40 +0800

    add

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export function test_push(){
-    let params = { "appId": "6159", "title": "this is nba app title", "body": "", "dateSent": new Date().toGMTString()};
+    let params = { "appId": "you-appid-from app.nativenotify.com", "title": "this is nba app title", "body": "", "dateSent": 'xx xx'};
     console.log(params);
     let headers = { "Content-Type": "application/json"};
     axios.post('https://app.nativenotify.com/api/notification', {params, headers}).then((resp) => {
